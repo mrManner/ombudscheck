9b7cdd34c57620d44bb4dbc8166354ed7b9a5009
mrmanner <johan.manner@scouterna.se>
2016-10-20 01:37:27 +0200

    Read the infile and guess the easy ones
    That is everything that doesn't require Scoutnet

--- ombud.py
+++ ombud.py
@@ -5,6 +5,27 @@ import argparse
 import requests
 from bs4 import BeautifulSoup as bs
 
+def getgroup(p):
+    if p['Huvudkar'] == 'Ja':
+        # Get group from Scoutnet
+        p['Representerar'] = scrape(p['Medlemsnummer'])
+        p['Bra gissning'] = True
+        return p
+    elif p['Annankar'] != '':
+        p['Representerar'] = p['Annankar']
+        p['Bra gissning'] = True
+        return p
+    else:
+        # This p has not indicated any group, so we guess 
+        # they represent their main group but mark them for reference
+        p['Representerar'] = scrape(p['Medlemsnummer'])
+        p['Bra gissning'] = False
+        return p
+    pass
+
+def scrape(num):
+    pass
+
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="The csv export file from Scoutnet")
 parser.add_argument("-u", "--username", required=True,
@@ -15,3 +36,5 @@ args = parser.parse_args()
 
 file = open(args.file, 'r')
 reader = csv.DictReader(file, delimiter=';')
+
+print([getgroup(p) for p in reader if 'Ombud' in p['Deltagaralternativ']])
